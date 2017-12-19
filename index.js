c51cc693be5405a126d03b1550bc4173a8b25c0a
DanielRSnell <hi@danielsnell.ninja>
2017-12-19 16:42:33 -0500

    callback

--- index.js
+++ index.js
@@ -6,23 +6,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var shopifyAPI = require('shopify-node-api');
 
-
 var Shopify = new shopifyAPI({
   shop: 'pollardschicken-com.myshopify.com', // MYSHOP.myshopify.com
   shopify_api_key: 'cb10241d405803d037ed2d06c4840015', // Your API key
   access_token: '0512819bc1724b742aabfcb605c97d12' // Your API password
 });
 
-const faxOrders = [];
 
 app.post('/order-recieved', function(req, res) {
-   var body = req.body
-   
-   const getHeader = req.get('x-shopify-order-id');
-   const fetchOrder = Shopify.get(`/admin/orders/${getHeader}`);
-   console.log(fetchOrder);
+   const faxOrders = [];
+   const id = req.get('x-shopify-order-id');
+   faxOrders.push(id)
+   sendFax(id)
+   res.send('success')
 });
 
+function sendFax(id) {
+  console.log('sending fax...')
+  // make the api call to send the fax
+}
 
 // Setting up heroku - Dynamic Binding
 const PORT = process.env.PORT || 2000;
