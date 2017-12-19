341e61691ff562a9ea08d344276bd618d6139063
DanielRSnell <hi@danielsnell.ninja>
2017-12-19 11:45:33 -0500

    test add

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.post('/order-recieved', function(req, res) {
+   var body = req.body.Body
+   res.set('Content-Type', 'text/plain')
+   res.send(`You sent: ${body} to Express`)
+});
+
+
+if (process.env.NODE_ENV === 'production') {
+	//This will serve production React Enviroment
+
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+// Setting up heroku - Dynamic Binding
+const PORT = process.env.PORT || 2000;
+app.listen(PORT);
