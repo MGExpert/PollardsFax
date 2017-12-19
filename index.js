82d1d7d3c23d5e6b0656e72ae21b76cd5e0f14cd
DanielRSnell <hi@danielsnell.ninja>
2017-12-19 13:18:57 -0500

    body parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/order-recieved', function(req, res) {
    var body = req.body
