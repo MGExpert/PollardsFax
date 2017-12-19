8eeb0f9209789d070884016c0dcfd637ebbbd7c8
DanielRSnell <hi@danielsnell.ninja>
2017-12-19 17:07:51 -0500

    log changes

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ var Shopify = new shopifyAPI({
   access_token: '0512819bc1724b742aabfcb605c97d12' // Your API password
 });
 
+  const idStore = [];
 
 app.post('/order-recieved', function(req, res) {
    const faxOrders = [];
    const id = req.get('x-shopify-order-id');
    faxOrders.push(id)
+   idStore.push(id)
    getDataFromShopify(id)
    res.send('success')
 });
 
 function getDataFromShopify(id) {
   console.log('getting data...')
+  console.log(`This is the id: ${id}`);
   // make the api call to get the data
   Shopify.get('/admin/orders/#' + id + '.json', function(err, data, headers) {
     console.log(data); // Data contains product json information
@@ -34,6 +37,8 @@ function sendFax() {
 console.log('sending fax...')
 // make the api call to send the fax
 }
+
+console.log(idStore);
 
 // Setting up heroku - Dynamic Binding
 const PORT = process.env.PORT || 2000;
